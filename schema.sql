594919231697053c9540363a118df4261e3936eb
jhernandez4 <jokhernandez120744@gmail.com>
2024-10-21 18:44:22 -0700

    Change to valid PLSQL syntax

--- schema.sql
+++ schema.sql
@@ -5,8 +5,24 @@
 -- 3. id/rowid needs to be "id serial primary key"
 -- 4. quoted strings in the CREATE INDEX need the quotes removed.
 
-CREATE TABLE containers (containerNumber varchar, nameOfShip varchar, containerSize float, dateContainerShipped varchar);
-INSERT INTO containers VALUES('123','COSCO Star',50,'2024-01-01');
-INSERT INTO containers VALUES('456J','MAERSK Rock',25.600000000000000532,'2024-03-08');
-INSERT INTO containers VALUES('x1','Betty',34,'2024-05-14');
+-- CREATE TABLE containers (containerNumber varchar, nameOfShip varchar, containerSize float, dateContainerShipped varchar);
+-- INSERT INTO containers VALUES('123','COSCO Star',50,'2024-01-01');
+-- INSERT INTO containers VALUES('456J','MAERSK Rock',25.600000000000000532,'2024-03-08');
+-- INSERT INTO containers VALUES('x1','Betty',34,'2024-05-14');
+-- CREATE UNIQUE INDEX idx_containers_containerNumber ON containers (containerNumber);
+
+CREATE TABLE containers (
+    id serial primary key,
+    containerNumber varchar(50) NOT NULL,
+    nameOfShip varchar(100) NOT NULL,
+    containerSize float NOT NULL,
+    dateContainerShipped date NOT NULL
+);
+
+INSERT INTO containers (containerNumber, nameOfShip, containerSize, dateContainerShipped) VALUES
+('123', 'COSCO Star', 50, '2024-01-01'),
+('456J', 'MAERSK Rock', 25.6, '2024-03-08'),
+('x1', 'Betty', 34, '2024-05-14');
+
 CREATE UNIQUE INDEX idx_containers_containerNumber ON containers (containerNumber);
+
